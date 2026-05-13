687b54d0818ceee89afe65e45d8ace76f3d903cd
Lindo-Mthombeni <ntsakomthombeni13@gmail.com>
2026-05-14 00:44:33 +0200

    Polshing...

--- client/src/components/NavBar.tsx
+++ client/src/components/NavBar.tsx
@@ -6,7 +6,10 @@ const NavBar = () => {
     <nav className="h-16 bg-base-300 not-dark:bg-card z-10">
       <div className="mx-auto my-auto max-w-[90%] py-2 px-1">
         <div className="flex items-center justify-between">
-          <h1 className="text-primary tracking-tight">ThinkBoard</h1>
+          <h1 className="text-primary tracking-tight">Note App</h1>
+          <h3 className="text-primary font-bold text-sm tracking-tight">
+            MADE BY LINDOKUHLE MTHOMBENI
+          </h3>
           <div className="flex items-center">
             <Link to="/create" className="btn btn-primary rounded-full">
               <PlusIcon className="size-5" /> <span>New Note</span>
